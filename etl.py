1c2ecbf3a8a8876629ccf02b6bf2ef9bf9298974
KelvinOyanna <dotkelplus@gmail.com>
2023-11-05 18:39:18 +0100

    updated changes

--- etl.py
+++ etl.py
@@ -34,19 +34,3 @@ def get_data_from_api():
     return crypto_price_data
 
 
-def read_from_azure_blob_storage(bucket_name, path):
-    objects_list = s3_client.blob_list(Bucket = bucket_name, Prefix = path) # List the objects in the bucket
-    file = objects_list.get('Contents')[1]
-    key = file.get('Key') # Get file path or key
-    obj = s3_client.get_object(Bucket = bucket_name, Key= key)
-    data = pd.read_csv(io.BytesIO(obj['Body'].read()))
-    return data
-
-def write_to_azure_blob_storage(data, bucket_name, folder):
-    file_name = f"crypto_price_data_{datetime.now().strftime('%Y%m%d')}.csv" # Create a file name
-    csv_buffer = StringIO() # Create a string buffer to collect csv string
-    data.to_csv(csv_buffer, index=False) # Convert dataframe to CSV file and add to buffer
-    # using the put_object(write) operation to write the data into s3
-    s3_client.put_object(Bucket=bucket_name, Key=f'{folder}/{file_name}', Body=csv_str ) 
-    return csv_buffer
-
