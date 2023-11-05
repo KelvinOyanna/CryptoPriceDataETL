f601d23145e0ab4bfc785f216626994c320985f2
KelvinOyanna <dotkelplus@gmail.com>
2023-11-05 17:51:08 +0100

    added a new function to write to an s3 bucket

--- etl.py
+++ etl.py
@@ -41,3 +41,11 @@ def read_from_s3(bucket_name, path):
     obj = s3_client.get_object(Bucket = bucket_name, Key= key)
     data = pd.read_csv(io.BytesIO(obj['Body'].read()))
     return data
+
+def write_to_s3(data, bucket_name, folder):
+    file_name = f"crypto_price_data_{datetime.now().strftime('%Y%m%d')}.csv" # Create a file name
+    csv_buffer = StringIO() # Create a string buffer to collect csv string
+    data.to_csv(csv_buffer, index=False) # Convert dataframe to CSV file and add to buffer
+    csv_str = csv_buffer.getvalue() # Get the csv string
+    # using the put_object(write) operation to write the data into s3
+    s3_client.put_object(Bucket=bucket_name, Key=f'{folder}/{file_name}', Body=csv_str ) 
